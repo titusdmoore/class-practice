3933a733ec9301e155813a96570aabe8a183bdf7
Titus Moore <titusdmoore@gmail.com>
2019-07-25 16:15:43 -0400

    Add project files.

---
+++ ClassPracticeProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassPracticeProject {
+    class Program {
+        static void Main(string[] args) {
+            Friend alex = new Friend("Alex", "Chan");
+            alex.age = 25;
+            Console.WriteLine($"My one and only friend is {alex.firstName} {alex.lastName} and he is {alex.age} years old\");
+        }
+    }
+    class Friend {
+        public int age { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+
+        public Friend( string FirstName, string LastName) {
+            this.firstName = FirstName;
+            this.lastName = LastName;
+        }
+    }
+}
